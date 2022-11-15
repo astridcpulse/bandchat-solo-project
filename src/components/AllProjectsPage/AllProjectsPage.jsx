db52d870af20b4cd97ebade7f03b222a9eb7bf5f
Astrid Pulse <astridcpulse@gmail.com>
2022-11-15 13:56:36 -0600

    begin building allprojectspage, and create new project button

--- src/components/AllProjectsPage/AllProjectsPage.jsx
+++ src/components/AllProjectsPage/AllProjectsPage.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { Button, OutlinedInput } from '@mui/material';
 
 function AllProjectsPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const [newProject, setNewProject]  = useState('');
+
+  const fetchProjects = () => {
+    //TODO create fetch projects GET_PROJECTS
+    //TODO put this fetchPrjects function in a useEffect
+  }
+
+  const postProject = () => {
+
+    console.log('newproj state', newProject);
+  }
+
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       {/* <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" /> */}
+      <OutlinedInput 
+        onChange={(evt) => setNewProject(evt.target.value)}
+        placeholder="new project name"
+      ></OutlinedInput>
+
+      <Button 
+        onClick={postProject}
+        variant="contained"
+      >
+        Create New Project
+      </Button>
     </div>
   );
 }
