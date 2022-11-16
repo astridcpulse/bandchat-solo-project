1c698120311cbe6456b588b2a31946c2d2a81dda
Astrid Pulse <astridcpulse@gmail.com>
2022-11-16 14:06:49 -0600

    route to project workspace established with correct project displayed

--- src/components/ProjectWorkspace/ProjectWorkspace.jsx
+++ src/components/ProjectWorkspace/ProjectWorkspace.jsx
@@ -1,10 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {useEffect, useState } from 'react';
+
+import { useParams } from 'react-router-dom';
+
+import { ThemeProvider, createMuiTheme, Typography } from '@mui/material';
 
 
 function ProjectWorkspace(){
+    const params = useParams();
+    const dispatch = useDispatch();
+
+    const project = useSelector(store => store.project)
+
+    const [thisProject, setThisProject] = useState('');
+
+    const chosenProject = (project.find((project) => {
+        if(project.id == params.id){
+            return project;
+        }
+    }))
+
+    useEffect(() => {
+        setThisProject(chosenProject);
+        dispatch({ type: 'FETCH_PROJECTS'});
+    }, [params.id])
+
 
     return (
         <>
-        
+            <Typography 
+                variant="h3"
+                align='center'
+            > {thisProject && thisProject.project_name}</Typography>
         </>
     );
 }
