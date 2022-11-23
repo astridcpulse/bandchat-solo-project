297607d15c3b1e82a5411584b2b8a7becd27ce8a
Astrid Pulse <astridcpulse@gmail.com>
2022-11-22 22:54:58 -0600

    remove post from sound. only delete will be left once made

--- server/routes/audio.router.js
+++ server/routes/audio.router.js
@@ -4,29 +4,11 @@ const router = express.Router();
 const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+
+
+router.delete('/', (req, res) => {
+  // UPDATE the sql 
 });
 
-/**
- * POST route template
- */
-router.post('/', rejectUnauthenticated, (req, res) => {
-  // POST route code here
-  const sqlText = `INSERT INTO "part_data" ("sound_url") VALUES $1;`
-  const sqlParams = req.body;
-  pool.query(sqlText, sqlParams)
-      .then((dbRes) => {
-        console.log('success in postAudio');
-        res.sendStatus(200);
-      })
-      .catch((err) => {
-        console.error('error in post', err);
-        res.sendStatus(500);
-      })
-});
 
 module.exports = router;
