5747c9de69d9f6794789f7d62d91cd499608ea2b
Astrid Pulse <astridcpulse@gmail.com>
2022-11-22 22:53:41 -0600

    remove post from chord router. only delete is left

--- server/routes/chord.router.js
+++ server/routes/chord.router.js
@@ -6,32 +6,6 @@ const {
   } = require('../modules/authentication-middleware');
 
 
-router.put('/', rejectUnauthenticated, (req, res) => {
-    console.log('req body', req.body.info)
-
-    const sqlText = `UPDATE "part_data"
-                    SET "chord_value" = $1,
-                        "chord_mode" = $2,
-                        "chord_text" = $3
-                    WHERE "part_data".id = $4;`;
-                    
-    const sqlParams = [ 
-                        req.body.info.noteVal,
-                        req.body.info.mode,
-                        req.body.info.text,
-                        req.body.partId
-                        ]
-
-    pool.query(sqlText, sqlParams)
-        .then((dbRes) => {  
-            res.sendStatus(200);
-        })
-        .catch((dbErr) => {
-            console.error('error in POST note', dbErr)
-            res.sendStatus(500);
-        });
-});
-
 router.delete('/:id', rejectUnauthenticated, (req, res) => {
     console.log('params for the part id ', req.params.id);
 
