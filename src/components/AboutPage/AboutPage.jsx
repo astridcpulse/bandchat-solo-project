e90c3aa6a89ef498f18a14fba83b2b192a475c3c
Astrid Pulse <astridcpulse@gmail.com>
2022-11-26 16:01:54 -0600

    styled about page further

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import {GitHub} from '@mui/icons-material';
+
 import {
   Box,
   Card,
   CardMedia,
-  Typography
+  Typography,
+  Link,
+  Stack,
+  Button,
+  IconButton
 } from '@mui/material';
 // This is one of our simplest components
 // It doesn't have local state,
@@ -11,17 +18,21 @@ import {
 // or even care what the redux state is'
 
 function AboutPage() {
+  const history = useHistory();
+
   return (
     <Box className="container">
-      <Box>
-        <Typography variant='h2' sx={{fontFamily: 'Rubik Glitch'}}> 
-          What is BandChat?
+      <Box
+        sx={{px:2, pt:7}}
+      >
+        <Typography variant='h4' sx={{fontFamily: 'Rubik Glitch'}}> 
+          What is a BandChat?
         </Typography>
         <Typography 
           sx={{m:3, }}
           variant='h4'
         >
-          BandChat is a workspace for musicians to collaborate together on their musical projects, without stepping on each others toes. Everyone can have their own workspace, and with BandChat you wont need to wait til rehearsal to learn what your drummer has been up to!
+          BandChat is a workspace for musicians to collaborate together on their musical projects without stepping on each other's toes. Everyone can have their own workspace, and with BandChat you wont need to wait til rehearsal to find out what your drummer has been up to!
         </Typography>
       </Box>
       <Card
@@ -29,26 +40,39 @@ function AboutPage() {
           display: 'flex',
           height: 350,
           width: 600,
-          boxShadow: 3,
-          p: 4
+          boxShadow: 5,
+          p: 4,
+          ml: 5
         }}
       >
+        <Stack>
         <Typography 
           sx={{m: 2}}
           variant='h5'
           color='text.secondary'
         > 
-          BandChat is soley developed and maintained by Astrid Pulse. She sings and plays guitar. Shes the total package. </Typography>
+          BandChat is solely developed and maintained by Astrid Pulse. She sings and plays guitar. She's the total package. </Typography>
+         
+          <Button
+          sx={{color: 'secondary.light', borderColor:'secondary.light'}}
+          variant='outlined' 
+          startIcon={<GitHub/>}
+          className='navLink' 
+          href='https://github.com/astridcpulse'>
+            GitHub
+          </Button>
+        </Stack>
         <CardMedia  
           sx={{
-          
             height: 300, 
             width: 800, 
-            border: 10,
-            borderRadius: 4
-
+            border: 5,
+            borderRadius: 4,
+            p:2
           }}
           image='images/my_linkedin_qr_code.jpeg'>
+          <Typography> LinkedIn </Typography>
+
           
         </CardMedia>
       </Card>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { 
-  // Link, 
-  useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
 
