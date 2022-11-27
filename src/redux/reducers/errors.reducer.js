70337c580abad756a5456784489033daab5e5772
Astrid Pulse <astridcpulse@gmail.com>
2022-11-27 12:22:41 -0600

    update error messages

--- src/redux/reducers/errors.reducer.js
+++ src/redux/reducers/errors.reducer.js
@@ -7,11 +7,11 @@ const loginMessage = (state = '', action) => {
     case 'CLEAR_LOGIN_ERROR':
       return '';
     case 'LOGIN_INPUT_ERROR':
-      return 'Enter your username and password!';
+      return 'Please enter your Username and Password';
     case 'LOGIN_FAILED':
-      return "Oops! The username and password didn't match. Try again!";
+      return "Ope! Username and password didn't match... try again!";
     case 'LOGIN_FAILED_NO_CODE':
-      return 'Oops! Something went wrong! Is the server running?';
+      return 'Oops! Sorry, something went wrong! Maybe the server?' 🤖;
     default:
       return state;
   }
@@ -24,9 +24,9 @@ const registrationMessage = (state = '', action) => {
     case 'CLEAR_REGISTRATION_ERROR':
       return '';
     case 'REGISTRATION_INPUT_ERROR':
-      return 'Choose a username and password!';
+      return 'Choose a (cool) username and password please 🙂';
     case 'REGISTRATION_FAILED':
-      return "Oops! That didn't work. The username might already be taken. Try again!";
+      return "Ope 😩! That Username might be taken, try something cool and new!";
     default:
       return state;
   }
